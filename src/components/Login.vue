1f6e19d8db1636121a857a69a093101b8a9e3767
Gabriel Peres Leopoldino <gabrielperes97@gmail.com>
2019-02-09 22:31:10 -0200

    Initial commit

---
+++ src/components/Login.vue
@@ -0,0 +1,120 @@
+<template>
+    <div>
+    <div style="height: 10%;"></div>
+    <div class="panel">
+        <div class="panel-content">
+        <h1>UniNote</h1>
+        <v-tabs 
+            centered
+            v-model="active"
+            slider-color="primary"
+            >
+            <v-tab :key="signin">
+                Sign In
+            </v-tab>
+            <v-tab-item :key="signin">
+            <v-card flat>
+                <v-card-text>
+                    <v-form 
+                        ref="form"
+                        v-model="valid"
+                        lazy-validation
+                    >
+                        <v-text-field
+                            prepend-icon="alternate_email"
+                            v-model="email"
+                            label="E-Mail"
+                            required
+                        ></v-text-field>
+                        <v-text-field
+                            prepend-icon="lock"
+                            v-model="password"
+                            :append-icon="showPassword ? 'visibility_off' : 'visibility'"
+                            :type="showPassword ? 'text' : 'password'"
+                            label="Password"
+                            @click:append="showPassword = !showPassword"
+                            required
+                        ></v-text-field>
+                        <v-btn @click="submit" color="primary">Sign-In</v-btn>
+                    </v-form>
+                </v-card-text>
+            </v-card>
+            </v-tab-item>
+            <v-tab :key="signup">
+                Sign up
+            </v-tab>
+            <v-tab-item :key="signup">
+                <v-card flat>
+                    <v-card-text>
+                        <v-form 
+                        ref="form"
+                        v-model="valid"
+                        lazy-validation
+                        >
+                            <v-text-field
+                                v-model="name"
+                                label="First Name"
+                                required
+                            ></v-text-field>
+                            <v-text-field
+                                v-model="name"
+                                label="Last Name"
+                                required
+                            ></v-text-field>
+                            <v-text-field
+                                v-model="email"
+                                label="E-mail"
+                                required
+                            ></v-text-field>
+                            <v-text-field
+                                v-model="password"
+                                :append-icon="showPassword ? 'visibility_off' : 'visibility'"
+                                :type="showPassword ? 'text' : 'password'"
+                                label="Password"
+                                @click:append="showPassword = !showPassword"
+                                required
+                            ></v-text-field>
+                            <v-btn @click="submit" color="primary">Sign-Up</v-btn>
+                        </v-form>
+                    </v-card-text>
+                </v-card>
+            </v-tab-item>
+        </v-tabs>
+        </div>
+    </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'login-component',
+  data() {
+      return {
+        showPassword: false
+      }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.panel{
+    margin: 0 auto;
+    padding: 30px;
+    width: 30%;
+    background-color: #FFF;
+}
+
+.panel-content {
+    text-align: center;
+}
+
+button {
+    width: 100%;
+}
+
+.forgot {
+    margin-top: 10px;
+    text-align: right;
+}
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app class="login"> 
+    <div>
+      <v-content>
+        <Login/>
+      </v-content>
+    </div>
+  </v-app>
+</template>
+
+<script>
+  import Login from '../components/Login'
+
+  export default {
+    components: {
+      Login
+    }
+  }
+</script>
+<style scoped>
+.login {
+    background-color: #082767;
+    background-image: url("../assets/background.jpeg");
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    background-size: cover;
+}
+</style>
