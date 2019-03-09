0526e6659ab10419d40f5735887a6c69a69a671d
Gabriel Peres Leopoldino <gabrielperes97@gmail.com>
2019-03-08 22:00:16 -0300

    Esboçando tela de dashboard

---
+++ src/components/Note.vue
@@ -0,0 +1,32 @@
+<template>
+<v-app class="note">
+    <v-card>
+        <v-card-title primary-title>
+            <div>
+                <h3>
+                    Título
+                </h3>
+                Teste
+            </div>
+        </v-card-title>
+    </v-card>
+</v-app>
+</template>
+
+<style lang="sass" scoped>
+.note
+    width: 100%
+</style>
+
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+    name: "note-component",
+    data() {
+    return {
+      showPassword: false
+    };
+  }
+});
+</script>
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,36 @@
+<template lang="html">
+<v-app class="Dashboard">
+  <v-container>
+    <v-layout row wrap>
+      <v-flex xl3 lg3 md6 sm6 xs12>
+        <Note/>
+      </v-flex>
+        <v-btn
+        fixed
+        dark
+        fab
+        bottom
+        right
+        color="pink"
+        >
+        <v-icon>add</v-icon>
+    </v-btn>
+    </v-layout>
+  </v-container>
+  </v-app>
+</template>
+
+<style lang="sass" scoped>
+.html
+    width: 100%
+</style>
+
+<script lang="ts">
+import Vue from 'vue';
+import Note from '../components/Note.vue';
+export default Vue.extend({
+    components: {
+        Note
+    }
+});
+</script>
